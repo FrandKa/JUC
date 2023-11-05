0979ee8702e6bf9f7c4117aab56fdfc484e21103
Frand ka kaslana <Frandkytj@outlook.com>
2023-11-05 14:37:37 +0800

    锁降级

---
+++ juc_base/src/main/java/com/kakaowo/juc_base/lessons02/Demo02.java
@@ -0,0 +1,40 @@
+package com.kakaowo.juc_base.lessons02;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @program: juc_part
+ * @description:
+ * @author: Mr.Ka
+ * @create: 2023-11-05 14:31
+ **/
+
+public class Demo02 {
+    public static void main(String[] args) {
+        ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+        ReentrantReadWriteLock.ReadLock readLock = rwLock.readLock();
+        ReentrantReadWriteLock.WriteLock writeLock = rwLock.writeLock();
+
+        // 锁降级: 可以在写操作的时候进行读操作, 提高性能;
+        writeLock.lock();
+        System.out.println("---write");
+        readLock.lock();
+        System.out.println("---read");
+        writeLock.unlock();
+        readLock.unlock();
+
+        // 读锁不可以升级成为写锁
+        readLock.lock();
+        System.out.println("---read");
+        // 不释放读锁就不可以使用写操作
+        writeLock.lock();
+        System.out.println("---write");
+        writeLock.unlock();
+        readLock.unlock();
+
+
+
+
+
+    }
+}
--- juc_base/src/main/java/com/kakaowo/juc_base/lessons02/DemoReadLock.java
+++ juc_base/src/main/java/com/kakaowo/juc_base/lessons02/DemoReadLock.java
@@ -11,6 +11,12 @@ import java.util.concurrent.FutureTask;
 
 public class DemoReadLock {
     public static void main(String[] args) {
+        // 容易出现线程饥饿问题:
+        // 读的时候不可以写, 但是写的时候可以读, 如果一直读就一直不可以写;
+        // 锁降级:
+        // 写入锁降级为读锁: (写的权限 > 读的权限)
+        // JDK8:
+        // 获得写锁 => 获得读锁 => 释放写锁(降级) => 释放读锁
         MyCache cache = new MyCache();
         for (int i = 0; i < 5; i++) {
             final int num = i;
